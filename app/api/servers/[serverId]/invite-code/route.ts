2896dd3f29e4f988673f4a342180172aaef7c750
Kurisani-web <kurisanim2@gmail.com>
2025-05-31 19:26:49 +0200

    Hope

--- app/api/servers/[serverId]/invite-code/route.ts
+++ app/api/servers/[serverId]/invite-code/route.ts
@@ -5,16 +5,16 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function PATCH(
   request: NextRequest,
-  context: { params: { serverId: string } } // or `any` as fallback
+  context: { params: Promise<{ serverId: string }> }
 ) {
   try {
+    const { serverId } = await context.params;
+
     const profile = await currentProfile();
 
     if (!profile) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
-
-    const { serverId } = context.params;
 
     if (!serverId) {
       return new NextResponse('Server ID is missing', { status: 400 });
@@ -35,4 +35,4 @@ export async function PATCH(
     console.error('[SERVER_ID]', error);
     return new NextResponse('Internal Server Error', { status: 500 });
   }
-        }
+      }
